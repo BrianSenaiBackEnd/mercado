f7d2e8df098794bd0bab8b41193e519ab4544c16
Brian-Fontes <pinheiro.portella@gamil.com>
2025-11-19 16:09:52 -0300

    novas adições em controller é service

--- src/main/java/br/com/senai/backend/sistema_mercado/controllers/FuncionarioController.java
+++ src/main/java/br/com/senai/backend/sistema_mercado/controllers/FuncionarioController.java
@@ -1,14 +1,19 @@
 package br.com.senai.backend.sistema_mercado.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.senai.backend.sistema_mercado.models.Funcionario;
+import br.com.senai.backend.sistema_mercado.models.Mercadoria;
 import br.com.senai.backend.sistema_mercado.services.FuncionarioService;
 
 
@@ -28,4 +33,23 @@ public class FuncionarioController {
     public Funcionario recuperarPorId(@PathVariable Integer id) {
         return funcionarioService.recuperarPorId(id);
     }
+    
+    @GetMapping("/listar-todos")
+    public List<Funcionario> listarTodos() {
+        return funcionarioService.listarTodos();
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public Funcionario atualizar(@PathVariable Integer id, @RequestBody Funcionario funcionario) {
+        return funcionarioService.atualizar(id, funcionario);
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public String deletarPorId(@PathVariable Integer id) {
+        if(funcionarioService.removerPorId(id)){
+            return "Funcionario removido com sucesso!";
+        }
+        return "Falha ao remover o funcionario.";
+    }
+       
 }
--- src/main/java/br/com/senai/backend/sistema_mercado/controllers/MercadoriaController.java
+++ src/main/java/br/com/senai/backend/sistema_mercado/controllers/MercadoriaController.java
@@ -1,15 +1,23 @@
 package br.com.senai.backend.sistema_mercado.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.senai.backend.sistema_mercado.models.Mercadoria;
 import br.com.senai.backend.sistema_mercado.services.MercadoriaService;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 @RestController
 @RequestMapping("/mercadoria")
@@ -27,4 +35,23 @@ public class MercadoriaController {
     public Mercadoria recuperarPorId(@PathVariable Integer id) {
         return mercadoriaService.recuperarPorId(id);
     }
+
+    @GetMapping("/listar-todos")
+    public List<Mercadoria> listarTodos() {
+        return mercadoriaService.listarTodos();
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public Mercadoria atualizar(@PathVariable Integer id, @RequestBody Mercadoria mercadoria) {
+        return mercadoriaService.atualizar(id, mercadoria);
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public String deletarPorId(@PathVariable Integer id) {
+        if(mercadoriaService.removerPorId(id)){
+            return "Mercadoria removida com sucesso!";
+        }
+        return "Falha ao remover a mercadoria.";
+    }
+       
 }
